fe8ea131431be7424db18debec5d543ee90a29af
Daniel <danielcoellovargas@gmail.com>
2025-12-05 16:53:19 -0500

    Add media files and update image display in templates
    Added new image files for courses, students, and trainers. Updated course, student, and trainer detail templates to display images with fixed width. Changed navigation links to use Django URL tags. Adjusted static and media root settings for better file management. Media serving in urls.py is now conditional on DEBUG mode.

--- assignment_02_week2/training_portal/urls.py
+++ assignment_02_week2/training_portal/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('academy/', include('academy.urls')),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
